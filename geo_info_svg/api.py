af105933c789bda045cba86ad6dfd39b568bd1a1
Stefan Brand <stefan.brand@eox.at>
2022-03-15 17:02:50 +0100

    Visualize geometries as info graphic

--- geo_info_svg/api.py
+++ geo_info_svg/api.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from sqlmodel import Session, select
+from geoalchemy2.types import Geometry
+from sqlmodel import Session, cast, select
 
 from .database import engine
 from .models import Country
@@ -17,8 +18,14 @@ async def test(request: Request):
             select(
                 Country.name,
                 Country.population,
+                cast(Country.geometry, Geometry).ST_XMin(),
+                cast(Country.geometry, Geometry).ST_YMin(),
+                cast(Country.geometry, Geometry).ST_XMax(),
+                cast(Country.geometry, Geometry).ST_YMax(),
                 Country.geometry.ST_AsSVG(),
             )
+            .order_by(Country.population.desc())  # type: ignore
+            .limit(10)
         ).all()
 
     return templates.TemplateResponse(
